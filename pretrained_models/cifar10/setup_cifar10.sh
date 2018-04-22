4890e7a0d7c1c0a609ced2ce4776f6bd0f83072e
Dany Haddad <danyhaddad43@gmail.com>
2018-04-21 22:33:08 -0500

    update cifar10 script

--- pretrained_models/cifar10/setup_cifar10.sh
+++ pretrained_models/cifar10/setup_cifar10.sh
@@ -23,7 +23,7 @@ if [ ! -d "data/cifar10_train" ]; then
     mkdir data/cifar10_train
 fi
 rm -f data/cifar10_train/*
-wget https://s3.amazonaws.com/robust-manifold-defense/cifar10_train.zip -O data/cifar10_train/data.zip
-chmod 775 data/cifar10_train/data.zip
-unzip data/cifar10_train/data.zip -d data/cifar10_train
-rm -f data/cifar10_train/data.zip
+wget -c https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -O data/cifar10_train/data.tar.gz
+chmod 775 data/cifar10_train/data.tar.gz
+tar -xvzf data/cifar10_train/data.tar.gz -C data/cifar10_train
+rm -f data/cifar10_train/data.tar.gz
